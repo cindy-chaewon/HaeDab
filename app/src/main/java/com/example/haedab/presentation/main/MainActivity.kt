92cc2769a1d0234a0a35ede6b17c2488d92f3e66
cindy <chaewon1769@daum.net>
2023-06-05 04:37:56 +0900

    프로젝트 세팅

---
+++ app/src/main/java/com/example/haedab/presentation/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.haedab.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.haedab.R
+import com.example.haedab.common.BaseActivity
+import com.example.haedab.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
