9ab74b51bb3c5ea0ff994c4ac46aa2757f474c7d
cindy <chaewon1769@daum.net>
2023-06-18 22:08:25 +0900

    Feat: 채팅 layout

--- app/src/main/java/com/example/haedab/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/haedab/presentation/main/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import com.example.haedab.R
 import com.example.haedab.common.BaseActivity
 import com.example.haedab.databinding.ActivityMainBinding
+import com.example.haedab.presentation.main.chatting.ChattingFragment
+import com.example.haedab.presentation.onboarding.OnboardingFirstFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame, ChattingFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/haedab/presentation/main/chatting/ChattingFragment.kt
@@ -0,0 +1,18 @@
+package com.example.haedab.presentation.main.chatting
+
+import android.os.Bundle
+import android.view.View
+import com.example.haedab.R
+import com.example.haedab.common.BaseFragment
+import com.example.haedab.databinding.FragmentChattingBinding
+import com.example.haedab.databinding.FragmentOnboardingFirstBinding
+import com.example.haedab.presentation.onboarding.OnboardingSecondFragment
+
+class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBinding::bind, R.layout.fragment_chatting)  {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
