c46821433b7d786d595bf2050fe116b1578e3023
cindy <chaewon1769@daum.net>
2023-07-04 21:16:23 +0900

    Feat: 이용약관 웹뷰

---
+++ app/src/main/java/com/example/haedab/presentation/main/setting/TermsFragment.kt
@@ -0,0 +1,39 @@
+package com.example.haedab.presentation.main.setting
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import com.example.haedab.R
+import com.example.haedab.common.BaseFragment
+import com.example.haedab.databinding.FragmentTermsBinding
+import com.example.haedab.presentation.main.chatting.ChattingActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+
+class TermsFragment: BaseFragment<FragmentTermsBinding>(FragmentTermsBinding::bind, R.layout.fragment_terms) {
+
+    var chattingActivity: ChattingActivity?=null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backBtn.setOnClickListener {
+            chattingActivity!!.back()
+        }
+
+        binding.webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+        }
+
+        binding.webview.loadUrl("https://docs.google.com/document/d/1fYL4_1dlwCgcAY7tKmVZu12mmtavRMmH3_YPuDy7KCo/edit?usp=sharing")
+
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        chattingActivity = context as ChattingActivity
+    }
+}
